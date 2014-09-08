1915f44724a6a262aaf8e9bb1eeeaf9fbee305bb
aliabdullah <ali.abdallah@bintricks.com>
2014-09-08 11:35:48 +0300

    add models into app.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var mongoose = require('mongoose');
-
+var fs = require('fs');
 mongoose.connect('mongodb://localhost/');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function callback() {
-	
+	fs.readdirSync(__dirname + '/models').forEach(function(filename) {
+		if(~filename.indexOf('.js')) {
+			require(__dirname + '/models/' + filename);
+		}
+	});
+
 	
 });
